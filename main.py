7b4beba7cec25678c6a4f6be3e395e2642295e34
Ben Schwarz <147880835+benSchwarzz@users.noreply.github.com>
2023-10-17 19:49:49 +0000

    this is a test

--- main.py
+++ main.py
@@ -3,8 +3,14 @@ import pygame as pg
 white = (255, 255, 255)
 
 def main():
-  
+  screen = pg.display.set_mode((400, 300))
+  screen.fill(white)
+  clock = pg.time.Clock()
 
+  while True:
+    pg.display.flip()
+
+    clock.tick(60)
   
-if __name__() == "__main__":
+if __name__ == "__main__":
   main()
